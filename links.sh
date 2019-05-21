b7c4c0e05949dbbd6e7db99f1d4d9a0276c70752
Jordan Leppert <jordanleppert@gmail.com>
2019-05-21 10:37:15 +0100

    Software changes, config fixes around scaling/flatpak

---
+++ links.sh
@@ -0,0 +1,45 @@
+#/bin/sh
+
+cd ~
+
+
+rm -rf Jordan
+ln -s /mnt/jordan Jordan
+
+rm -rf Documents
+ln -s Jordan/Documents Documents
+
+rm -rf Scripts
+ln -s Jordan/Scripts Scripts
+
+rm -rf Personal
+ln -s Jordan/Personal Personal
+
+rm -rf Stuff
+ln -s Jordan/Personal/Stuff Stuff
+
+rm -rf AppData
+ln -s Jordan/AppData AppData
+
+
+rm -rf Downloads
+ln -s /mnt/download Downloads
+
+rm -rf Public
+ln -s /mnt/public Public
+
+
+rm -rf Music
+ln -s /mnt/multimedia/Music Music
+
+rm -rf Videos
+ln -s /mnt/multimedia/Videos Videos
+
+rm -rf Pictures
+ln -s /mnt/multimedia/Pictures Pictures
+
+rm -rf Podcasts
+ln -s /mnt/multimedia/Podcasts Podcasts
+
+mkdir Temp
+
