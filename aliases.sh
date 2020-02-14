4cbd82b6b239b6858a7b85e0c03a14e9d6f05071
Jordan Leppert <jordanleppert@gmail.com>
2020-02-14 17:11:30 +0000

    VirtualBox alias

--- aliases.sh
+++ aliases.sh
@@ -5,3 +5,6 @@ alias ksp="perl /home/jordan/Scripts/ksp.pl"
 
 # Minecraft
 alias mcbackup="sh /home/jordan/Scripts/mc_archive.sh"
+
+# Virtual Box
+alias virtualboxXwayland="QT_QPA_PLATFORM=xcb VirtualBox"
