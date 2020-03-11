b35a48ddb0b52557faf415cff7317517dfa8f3d7
Jordan Leppert <jordanleppert@gmail.com>
2020-03-11 18:35:27 +0000

    Using pip to install softwareinstaller

--- install_arch_user.sh
+++ install_arch_user.sh
@@ -34,13 +34,16 @@ yay --noconfirm -S systemd-boot-pacman-hook
 
 ### Apps ###
 
+# Python
+sudo pacman --noconfirm -S python-pip
+
 # Flatpak
 sudo pacman --noconfirm -S flatpak # This should auto-select xdg-desktop-portal-gdk
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak --user remote-add --if-not-exists jordan-repo /mnt/flatpakrepo/repo --gpg-import=/mnt/flatpakrepo/repo/jordan-repo.gpg
 
 # Timeshift
-sudo pacman  --noconfirm -S cronie
+sudo pacman --noconfirm -S cronie
 yay --noconfirm -S timeshift
 sudo systemctl enable cronie.service
 sudo cp /home/jordan/GitRepo/LinuxSetup/timeshift-* /etc/cron.d/
@@ -49,4 +52,4 @@ uuid=`lsblk -o UUID -n $ROOT | perl -pe 'chomp' -`
 sudo sh -c "cat /home/jordan/GitRepo/LinuxSetup/timeshift.json | sed -e \"s/ROOT_DEVICE_UUID/$uuid/\" > /etc/timeshift.json"
 
 # Software Installer
-sudo ln -snf /home/jordan/GitRepo/SoftwareInstaller/softwareinstaller-run /usr/bin/si
+sudo pip3 install /home/jordan/GitRepo/SoftwareInstaller
