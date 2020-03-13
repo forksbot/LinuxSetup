05a0a225888f34d155333bd03d488fd481377dc7
Jordan Leppert <jordanleppert@gmail.com>
2020-03-13 18:03:40 +0000

    Major refresh of Wilfred config, sway install script, systemd, wofi, wlogout

---
+++ Wilfred/install_sway.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/sh
+
+
+### SWAY ###
+
+# Install
+sudo pacman --noconfirm -S sway swayidle bemenu gtk-layer-shell grim jq xorg-server-xwayland qt5-wayland pulseaudio pamixer pavucontrol gsimplecal nm-connection-editor lxtask-gtk3 termite
+#TODO remove waybar-git and install waybar via pacman when 0.9.2 is released
+yay --noconfirm -S wofi waybar-git wlogout redshift-wlr-gamma-control
+
+# Config
+cp -r /home/jordan/GitRepo/LinuxSetup/Wilfred/config/* /home/jordan/.config/
+cp /home/jordan/GitRepo/LinuxSetup/Sway/_.Xresources /home/jordan/.Xresources
+
+# Enable services
+systemctl --user enable waybar
+systemctl --user enable swayidle@1800
+systemctl --user enable redshift@4500
+
+# Startup
+sudo cp /home/jordan/GitRepo/LinuxSetup/Sway/sway@.service /etc/systemd/system/
+sudo cp /home/jordan/GitRepo/LinuxSetup/Sway/sway-debug@.service /etc/systemd/system/
+sudo systemctl enable sway@1
+
+
+### THEME ###
+
+sudo pacman --noconfirm -S breeze breeze-gtk xsettingsd plasma-integration
+gsettings set org.gnome.desktop.interface gtk-theme 'Breeze-Dark'
+gsettings set org.gnome.desktop.interface icon-theme 'breeze-dark'
+flatpak --user install -y Breeze-Dark
+cp /home/jordan/GitRepo/LinuxSetup/Sway/_.xsettingsd /home/jordan/.xsettingsd
+systemctl --user enable --now xsettingsd
+
+
+### FONTS ###
+
+sudo pacman --noconfirm -S gsfonts ttf-dejavu noto-fonts-emoji otf-font-awesome
+gsettings set org.gnome.desktop.interface font-name "Noto Sans Regular 10"
+yay --noconfirm -S ttf-ms-fonts fontconfig-ubuntu cairo-ubuntu
+fc-cache -f -v
+sudo pacman --noconfirm -S gnome-settings-daemon # Fixes GTK flatpaks font rendering via wayland
+
+
+### APPS ###
+
+# General
+sudo pacman --noconfirm -S thunar thunar-archive-plugin file-roller tumbler ristretto firefox mpv kcharselect
+flatpak --user install -y flathub org.libreoffice.LibreOffice com.sublimetext.three org.kde.okular
+
+# Modular calculator
+flatpak --user install jordan-repo io.github.jordanl2.ModularCalculator -y
